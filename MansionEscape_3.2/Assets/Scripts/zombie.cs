98153c28f68a47ea7ac3c516a93a0ade8f885362
Greg Rozmarynowycz <crashtest1@live.com>
2016-03-10 20:46:39 -0500

    zombie attacking behavior

--- MansionEscape_3.2/Assets/Scripts/zombie.cs
+++ MansionEscape_3.2/Assets/Scripts/zombie.cs
@@ -8,8 +8,10 @@ public class zombie : MonoBehaviour {
     public float speed;
 
     Combatant combatant;
+    public Combatant target;
     Animator anim;
     int direction = 1;
+    float attackCooldown = 0;
 
     Vector3 velocity;
     float moveTimer = 0;
@@ -21,11 +23,21 @@ public class zombie : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        moveTimer += Time.deltaTime;
+        
+        if(attackCooldown > 0)
+        {
+            anim.SetBool("Attacking", false);
+            attackCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            moveTimer += Time.deltaTime;
+        }
+        
         velocity = Vector3.zero;
         float period = 5;
 
-        if((moveTimer + period/2) % (period * 2) > period)
+        if((moveTimer + period/2) % (period * 2) > period && attackCooldown <= 0)
         {
             velocity = new Vector3(speed, 0, 0) * Mathf.Sign(Mathf.Sin(moveTimer/period)) * Time.deltaTime;
         }
@@ -37,6 +49,18 @@ public class zombie : MonoBehaviour {
         {
             Flip();
         }
+
+        Vector3 disp = transform.position - target.transform.position;
+        if (disp.magnitude < 2 && attackCooldown <= 0)
+        {
+            anim.SetBool("Attacking", true);
+            attackCooldown = 5;
+            if(Mathf.Sign(disp.x) == direction)
+            {
+                Flip();
+            }
+        }
+            
 	}
 
     //flip character through scaling
