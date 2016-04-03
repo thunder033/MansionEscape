1b15b06197c7fa09a6acb87a43f3857a7815b7ae
Greg Rozmarynowycz <crashtest1@live.com>
2016-04-02 21:48:08 -0400

    add health bar, and remaining player animation states

---
+++ MansionEscape_3.2/Assets/Scripts/Menu/healthBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class healthBar : MonoBehaviour {
+
+    public characterController2D target;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = transform.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.Play("bar", -1, target.health / 100.0f);
+        anim.speed = 0;
+	}
+}
