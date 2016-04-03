eab4e03b5fa39af14ae6eb57c37a74488f2fca5f
Greg Rozmarynowycz <crashtest1@live.com>
2016-04-02 22:20:09 -0400

    HUD shake on damage

--- MansionEscape_3.2/Assets/Scripts/Menu/healthBar.cs
+++ MansionEscape_3.2/Assets/Scripts/Menu/healthBar.cs
@@ -5,15 +5,25 @@ using System.Collections;
 public class healthBar : MonoBehaviour {
 
     public characterController2D target;
+    public Animator HUDAnimate;
+
     private Animator anim;
+    private float lastHealth;
 
 	// Use this for initialization
 	void Start () {
         anim = transform.GetComponent<Animator>();
+        lastHealth = target.health;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if(target.health < lastHealth)
+        {
+            lastHealth = target.health;
+            HUDAnimate.Play("shake", 0, 0);
+        }
+
         anim.Play("bar", -1, target.health / 100.0f);
         anim.speed = 0;
 	}
